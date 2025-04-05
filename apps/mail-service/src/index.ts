c7de5376b4352c6d878d321f6b81ab921db24915
Sidharth <sidharthinfernal@gmail.com>
2025-04-06 00:36:36 +0530

    implemented resend mail service

--- apps/mail-service/src/index.ts
+++ apps/mail-service/src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import {ResendAdapter} from "./adapters/resend-adapter.ts";
+import {MailService} from "./services/mail-service.ts";
+import {EmailQueue} from "./services/queue.ts";
+
+type Config = {
+    resendApiKey : string;
+    defaultFrom : string;
+    redisUrl : string;
+}
+
+export async function createMailService( { resendApiKey,defaultFrom,redisUrl } : Config) : Promise<MailService>{
+    const mailProvider = new ResendAdapter(resendApiKey,defaultFrom);
+
+    const emailQueue = new EmailQueue(redisUrl);
+
+    const mailService = new MailService(mailProvider,emailQueue,defaultFrom);
+
+    emailQueue.process(async (item) => {
+        await mailService.processQueueItem(item);
+
+    })
+
+    return mailService
+
+}
--- apps/mail-service/src/services/mail-service.ts
+++ apps/mail-service/src/services/mail-service.ts
@@ -1,3 +1,7 @@
+import type {EmailOptions, MailProvider} from "./mail-interface.ts";
+import type {EmailQueue} from "./queue.ts";
+import {renderTemplate} from "../templates";
+
 export type NotificationType =
     | 'monitor-down'
     | 'monitor-up'
@@ -9,8 +13,55 @@ export interface NotificationOptions {
     type: NotificationType;
     data: Record<string, any>;
     to: string | string[];
-    cc?: string | string[];
-    bcc?: string | string[];
+
 }
 
-import type {MailProvider,EmailOptions} from "./mail-interface.ts";
+
+export class MailService{
+    private provider : MailProvider;
+    private queue  : EmailQueue;
+    private defaultFrom : string;
+
+    constructor(provider : MailProvider, queue : EmailQueue, defaultFrom : string) {
+        this.provider = provider;
+        this.queue = queue;
+        this.defaultFrom = defaultFrom;
+    }
+
+    async sendMail(options : EmailOptions) : Promise<{ id : string} | Error>{
+        return this.provider.sendMail({
+            ...options,
+            from : options.from || this.defaultFrom,
+        });
+    }
+
+    async sendNotification(options : NotificationOptions) : Promise<void> {
+        const { to,type,data } = options;
+
+        await this.queue.add({
+            type,
+            data : {
+                to,
+                ...data
+            }
+        })
+    }
+
+    async processQueueItem(item : { type : NotificationType,data : any }) : Promise<void>{
+        const { type,data } = item;
+        const { to,...templateData } = data
+
+        const emailContent = await renderTemplate(type,templateData);
+
+        const result = await this.sendMail({
+            to,
+            subject : emailContent.subject,
+            html : emailContent.html,
+            text : emailContent.text,
+        })
+
+        if(result instanceof Error){
+            throw result
+        }
+    }
+}
